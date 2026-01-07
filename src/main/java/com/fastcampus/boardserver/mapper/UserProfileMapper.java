4ec052e42d82fe349f607b607deb0c002a967f9d
wonotter <zjaxhfl99@kumoh.ac.kr>
2026-01-08 01:45:04 +0900

    feat(service): 서비스 로직 추가
    - UserService, UserServiceImpl, DuplicateIdException
    회원가입, 로그인, 프로필 조회, 비밀번호 변경, 계정 삭제 기능 추가 및 예외 처리
    
    - UserProfileMapper
    DB에 SQL 쿼리를 매핑하는 인터페이스

---
+++ src/main/java/com/fastcampus/boardserver/mapper/UserProfileMapper.java
@@ -0,0 +1,28 @@
+package com.fastcampus.boardserver.mapper;
+
+import com.fastcampus.boardserver.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserProfileMapper {
+
+    public UserDTO getUserProfile(@Param("id") String userId);
+
+    int insertUserProfile(@Param("id") String id, @Param("password") String password,
+                          @Param("name") String name, @Param("phone") String phone,
+                          @Param("address") String address, @Param("createTime") String createTime,
+                          @Param("updateTime") String updateTime);
+
+    int deleteUserProfile(@Param("id") String id);
+
+    public UserDTO findByIdAndPassword(@Param("id") String id, @Param("password") String password);
+
+    int idCheck(@Param("id") String id);
+
+    public int updatePassword(UserDTO user);
+
+    public int updateAddress(UserDTO user);
+
+    int register(UserDTO userDTO);
+}
---
+++ src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
@@ -0,0 +1,85 @@
+package com.fastcampus.boardserver.service.impl;
+
+import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.exception.DuplicateIdException;
+import com.fastcampus.boardserver.mapper.UserProfileMapper;
+import com.fastcampus.boardserver.service.UserService;
+import com.fastcampus.boardserver.utils.SHA256Util;
+import java.util.Date;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserProfileMapper userProfileMapper;
+
+    public UserServiceImpl(UserProfileMapper userProfileMapper) {
+        this.userProfileMapper = userProfileMapper;
+    }
+
+    @Override
+    public void register(UserDTO userDTO) {
+        boolean dupleIdResult = isDuplicatedId(userDTO.getUserId());
+
+        if (dupleIdResult) {
+            throw new DuplicateIdException("중복된 아이디입니다.");
+        }
+        userDTO.setCreateTime(new Date());
+        userDTO.setPassword(SHA256Util.encryptSHA256(userDTO.getPassword()));
+
+        int insertCount = userProfileMapper.register(userDTO);
+
+        if (insertCount != 1) {
+            log.error("insertMember ERROR! {}", userDTO);
+            throw new RuntimeException(
+                    "insertUser ERROR! 회원가입 메서드를 확인해주세요\n" + "Params : " + userDTO);
+        }
+    }
+
+    @Override
+    public UserDTO login(String id, String password) {
+        String encryptPassword = SHA256Util.encryptSHA256(password);
+        return userProfileMapper.findByIdAndPassword(id, encryptPassword);
+    }
+
+    @Override
+    public boolean isDuplicatedId(String id) {
+        return userProfileMapper.idCheck(id) == 1;
+    }
+
+    @Override
+    public UserDTO getUserInfo(String userId) {
+        return userProfileMapper.getUserProfile(userId);
+    }
+
+    @Override
+    public void updatePassword(String id, String beforePassword, String afterPassword) {
+        String cryptoPassword = SHA256Util.encryptSHA256(beforePassword);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+
+        if (memberInfo != null) {
+            memberInfo.setPassword(SHA256Util.encryptSHA256(afterPassword));
+            int insertCount = userProfileMapper.updatePassword(memberInfo);
+        } else {
+            log.error("updatePassword ERROR! {}", memberInfo);
+            throw new IllegalArgumentException("updatePassword ERROR! 비밀번호 변경 메서드를 확인해주세요\n" + "Params : " + memberInfo);
+        }
+    }
+
+    @Override
+    public void deleteId(String id, String password) {
+        String cryptoPassword = SHA256Util.encryptSHA256(password);
+        UserDTO memberInfo = userProfileMapper.findByIdAndPassword(id, cryptoPassword);
+
+        if (memberInfo != null) {
+            userProfileMapper.deleteUserProfile(memberInfo.getUserId());
+        } else {
+            log.error("deleteId ERROR! {}", memberInfo);
+            throw new RuntimeException("deleteId ERROR! id 삭제 메서드를 확인해주세요\n" + "Params : " + memberInfo);
+        }
+    }
+}
